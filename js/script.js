dedba9104a1a65156b4c95184e895e5fdfc48487
maicol <maicol.matteoli@gmail.com>
2021-06-23 17:43:36 +0200

    add : inserisci + elimina functions

--- js/script.js
+++ js/script.js
@@ -3,14 +3,33 @@ const app = new Vue(
         el:"#app",
 
         data:{
+            tasks : [
+                "chiamare thorin",
+                "prenotare la cena da bilbo",
+                "trovare la porta segreta",
+                "recuperare l'archengemma",
+            ],
             
-                
+            nuovaTask : '',
             
         },
         
         methods: {
-              test(){
-                  alert("sono vue");
-              }         
+            //   test(){
+            //       alert("sono vue");
+            //   }
+            elimina(indice){
+                this.tasks.splice(indice,1);
+            },
+            
+            inserisci(){
+                // this.tasks.push()
+                console.log(this.nuovaTask.length);
+                if(this.nuovaTask.length > 0 || this.nuovaTask == ' '){
+                    this.tasks.push(this.nuovaTask);
+                    this.nuovaTask = '';
+                }
+                
+            }
         },
  });
