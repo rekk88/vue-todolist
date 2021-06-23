bf2150344c755665bd18057e5d85f38a083dba91
maicol <maicol.matteoli@gmail.com>
2021-06-23 17:06:43 +0200

    test vue

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const app = new Vue(
+    {
+        el:"#app",
+
+        data:{
+            
+                
+            
+        },
+        
+        methods: {
+              test(){
+                  alert("sono vue");
+              }         
+        },
+ });
